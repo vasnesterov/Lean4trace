879241253664f4ab9e3c5f00b5115322485492f8
Vasily Nesterov <vas.nesterov63@gmail.com>
2024-02-13 01:00:32 +0300

    fix

--- src/Lean/Elab/Tactic/Basic.lean
+++ src/Lean/Elab/Tactic/Basic.lean
@@ -65,7 +65,6 @@ def pruneSolvedGoals : TacticM Unit := do
 def getUnsolvedGoals : TacticM (List MVarId) := do
   pruneSolvedGoals
   getGoals
-
 @[inline] private def TacticM.runCore (x : TacticM α) (ctx : Context) (s : State) : TermElabM (α × State) :=
   x ctx |>.run s
 
@@ -231,21 +230,41 @@ where
         stxKind := autoStx.raw.getKind.toString
       }
 
-      let s ← Tactic.saveState
-      let traceState ← getTraceState -- We do backtrack trace message
-      try
-        withAtLeastMaxRecDepth 32768 <| withoutModifyingEnv <|
-          withOptions (fun o => o.setBool `_doTracing false) <| evalTactic autoStx
-        if (← getUnsolvedGoals).isEmpty then
-          IO.println "auto has closed the goal!"
-          IO.println (toJson ti).pretty
-        else
-          IO.println "useless auto : else"
-      catch _ =>
-        IO.println "useless auto : catch"
-      finally
-        s.restore (restoreInfo := true)
-        setTraceState traceState
+      let mhb := (← read (m := CoreM)).maxHeartbeats
+
+      let coreState := ← get (m := CoreM)
+      let metaState := ← get (m := MetaM)
+      let elabState := ← get (m := TermElabM)
+      let tacticState := ← get
+
+      let coreCtx := ← read (m := CoreM)
+      let metaCtx := ← read (m := MetaM)
+      let elabCtx := ← read (m := TermElabM)
+      let tacticCtx := ← read
+
+      withCurrHeartbeats do
+      withReader (m := CoreM) (fun ctx => { ctx with maxHeartbeats := mhb / 4 }) do
+      -- withReader (m := CoreM) (fun ctx => ctx) do
+      MetaM.run' (ctx := metaCtx) (s := metaState) do
+      Term.TermElabM.run' (ctx := elabCtx) (s := elabState) do
+      TacticM.runCore' (ctx := tacticCtx) (s := tacticState) do
+        let s ← Tactic.saveState
+        let traceState ← getTraceState -- We do backtrack trace message
+        try
+          withAtLeastMaxRecDepth 32768 <| withoutModifyingEnv <|
+            withOptions (fun o =>
+              o.setBool `_doTracing false) <|
+            evalTactic autoStx
+          if (← getUnsolvedGoals).isEmpty then
+            IO.println "auto has closed the goal!"
+            IO.println (toJson ti).pretty
+          else
+            IO.println "useless auto : else"
+        catch _ =>
+          IO.println "useless auto : catch"
+        finally
+          s.restore (restoreInfo := true)
+          setTraceState traceState
 
       -- IO.println s!"inside checkAuto: out"
 
